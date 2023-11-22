2199975418ddd9ef8c01bfc1608b7d33259d6df5
Ester Morales <estermorales.business@gmail.com>
2023-11-21 22:43:04 -0300

    alteracao layout

--- app/Views/livros.php
+++ app/Views/livros.php
@@ -10,15 +10,45 @@
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,600,0,0" />
 </head>
 
-<body class="font-body mx-6 my-3">
-    <header class="p-5">
-        <div
-            class="before:block before:absolute before:-inset-1 before:-skew-y-2 before:bg-blue-700 before:rounded-sm relative inline-block shadow-xl ">
-            <span class="text-3xl font-semibold relative text-slate-100 p-1"><a href="/">marcapágina.</a></span>
+<body class="font-body absolute inset-0 h-full w-full bg-gray-50 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+    <header>
+        <!-- <div>
+            <span class="text-4xl font-semibold relative text-slate-50 p-1 underline decoration-indigo-500"><a href="/">readtrackr</a></span>
+        </div> -->
+        <div class=" my-4 mx-6">
+            <img src="<?= base_url() ?>/assets/readtrackr.png" alt="" width="300">
         </div>
     </header>
     <main class="flex justify-center py-3">
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg border">
+        <div class="relative overflow-x-auto shadow-md  border ">
+            <div class="flex justify-between bg-white py-3 px-6">
+                <div class="bg-white py-3 px-6">
+                    <h1 class="text-4xl font-semibold text-gray-700">LEITURAS</h1>
+                </div>
+                <div class="flex items-center gap-4">
+                <form>   
+                    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </div>
+                        <input type="search" id="default-search" class="block w-full p-2 ps-10 text-sm text-gray-900 border rounded bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Pesquisar..." required>
+                    </div>
+                </form>
+                <button class="inline-flex items-center px-4 py-2 bg-gray-50 hover:bg-gray-100 text-gray-800 text-sm font-medium rounded border">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" style="margin-right:4px; fill: #6b7280">
+                    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M3.9 54.9C10.5 40.9 24.5 32 40 32H472c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9V448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6V320.9L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z"/></svg>
+                    Filtros
+                </button>
+                <button class="inline-flex items-center px-4 py-2 bg-blue-600 hover:bg-blue-700 text-gray-50 text-sm font-medium rounded border add-button">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" style="margin-right:4px; fill: #f9fafb">
+                    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
+                    Adicionar
+                </button>
+                </div>
+            </div>
             <table class="table-auto border-collapse rounded-xl">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                     <tr>
@@ -31,7 +61,7 @@
                         <th scope="col" class="px-6 py-3"></th>
                     </tr>
                 </thead>
-                <tbody class="bg-white border-b cursor-pointer text-gray-600">
+                <tbody class="bg-white border-b cursor-pointer text-gray-600 ">
                     <?php foreach ($livros as $livro): ?>
                     <tr class="hover:bg-gray-100 book" data-id="<?php echo $livro->ID; ?>">
                         <th scope="row" class="px-6 py-4 font-medium  whitespace-nowrap"><?php echo $livro->Titulo; ?></th>
@@ -49,7 +79,7 @@
                             <div class="relative inline-flex items-center justify-center progress-container">
                                 <!-- Building a Progress Ring: https://css-tricks.com/building-progress-ring-quickly/ -->
                                 <svg class="w-20 h-20">
-                                    <circle class="stroke-current text-gray-300" stroke-width="5" fill="transparent" r="30" cx="40" cy="40" />
+                                    <circle class="stroke-current text-gray-200" stroke-width="5" fill="transparent" r="30" cx="40" cy="40" />
                                     <circle class="stroke-current text-blue-600 progress-ring" stroke-width="5" stroke-linecap="round"
                                         :stroke="`blue-500`"
                                         :stroke-dasharray="`${Math.max(0, currentPercent)} ${Math.max(0, 100 - currentPercent)}`"
@@ -66,28 +96,22 @@
         </div>
     </main>
     <div class="flex justify-center gap-x-1">
-            <!-- Previous Button -->
-            <a href="#" class="flex items-center justify-center px-3 h-8 me-3 text-sm text-gray-600 bg-white  rounded-lg hover:bg-gray-100 hover:text-gray-700 ">
-                <svg class="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
-                </svg>
-                Página anterior
-            </a>
-            <a href="#" class="flex items-center justify-center px-3 h-8 text-sm text-gray-600 bg-white  rounded-lg hover:bg-gray-100 hover:text-gray-700 ">
-                Próxima página
-                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a>
-        </div>
-    <aside>
-        <div>
-            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 rounded-full fixed bottom-10 right-10 
-     shadow-xl add-button"><span
-                    class="material-symbols-outlined align-middle text-5xl p-2 font-bold"
-                    style="font-size: 32px;">add</span></button>
-        </div>
-    </aside>
+        <nav class="flex justify-center items-center gap-x-1">
+            <button type="button" class="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none">
+                <svg class="flex-shrink-0 w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg>
+                <span aria-hidden="true" class="sr-only">Previous</span>
+            </button>
+            <div class="flex items-center gap-x-1">
+                <button type="button" class="min-h-[38px] min-w-[38px] flex justify-center items-center border bg-gray-50 border-gray-200 text-gray-800 py-2 px-3 text-sm rounded focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" aria-current="page">1</button>
+                <button type="button" class="min-h-[38px] min-w-[38px] flex justify-center items-center border border-transparent text-gray-800 hover:bg-gray-100 py-2 px-3 text-sm rounded focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none">2</button>
+                <button type="button" class="min-h-[38px] min-w-[38px] flex justify-center items-center border border-transparent text-gray-800 hover:bg-gray-100 py-2 px-3 text-sm rounded focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none">3</button>
+            </div>
+            <button type="button" class="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none">
+                <span aria-hidden="true" class="sr-only">Next</span>
+                <svg class="flex-shrink-0 w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+            </button>
+        </nav>                    
+    </div>
     <!-- modals -->
     <?php include('_add_modal.php'); ?>
     <?php include('_edit_modal.php'); ?>
